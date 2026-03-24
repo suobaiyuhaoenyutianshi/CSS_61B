8afdb65b42364748cee3ed91d1c6c53262dded14
suobaiyuhaoenyutianshi <2467395920@qq.com>
2026-03-24 08:42:06 +0800

    前后反了

--- proj1/deque/LinkedListDepque.java
+++ proj1/deque/LinkedListDepque.java
@@ -1,146 +1,112 @@
 package deque;
 import java.util.Iterator;
-public class LinkedListDepque<T>  implements Iterable{
-//节点内部结构
-    private static class Node<I>{
-        I item;
-        Node<I> next;
-        public Node(I item, Node<I> next){
-            this.item = item;
-            this.next = next;
+import java.util.List;
+
+public class LinkedListDepque<T>{
+    //内部节点
+    private static class Node<T>{
+        T item;
+        Node prev;
+        Node next;
+
+        public Node(T item,Node prev,Node next){
+                this.item = item;
+                this.prev = prev;
+                this.next = next;
         }
     }
 
-    //链表是私有变量
-    private int size;//列表长度
-    private Node<T> sentry;//哨兵
-    private Node<T> tail;//跟着尾巴
-    //创建空链表
+    //哨兵
+    private Node<T> sentine;
+    private int size;
+    //构造
     public LinkedListDepque(){
+        sentine = new Node<>(null,null,null);
+        sentine.prev = sentine;
+        sentine.next = sentine;
         this.size = 0;
-        this.sentry = new Node<>(null,null);
-        this.tail = null;
     }
-
-//判空
-    public boolean isEmpty(){
-        if(size == 0)return true;
-        return false;
-    }
-
-    //添加尾巴
+    //addLast
     public void addLast(T x){
-        if(this.tail == null){
-            this.sentry.next = new Node<>(x,null);
-            this.tail = this.sentry.next;
-        }
-        else{
-            Node<T> newNode = new Node<>(x,null);
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-
+        Node<T> newNode = new Node<>(x,sentine.next,sentine);
+        newNode.prev.next = newNode;
+        this.sentine.next = newNode;
+        if(this.size==0)this.sentine.prev = newNode;
         this.size++;
     }
+    //addFirst
     public void addFirst(T x){
-
-        if(this.size == 0){
-              addLast(x);
-        }
-        else{
-            insert(x,0);
-        }
-
-    }
-    //返回第一个元素
-    public T getFirst(){
-        return this.sentry.next.item;
-    }
-    //返回最后一个元素
-    public T getLast(){
-        return this.tail.item;
-    }
-
-    public void insert(T x , int position){
-        if( position < 0 || position >= this.size){
-            throw new IndexOutOfBoundsException("你的插入范围 0<=x<=thi.size");
-
-        }
-        //需要找到position的前个位置
-        Node<T> prevItem = findNode(position - 1);
-        //创建新节点
-        Node<T> newNode = new Node<>(x,null);
-        newNode.next = prevItem.next;
-        prevItem.next = newNode;
-        if(this.size == position){
-            this.tail = newNode;
-        }
+        Node<T> newNode = new Node<>(x,sentine,sentine.prev);
+        newNode.next.prev = newNode;
+        this.sentine.prev = newNode;
+        if (this.size == 0)this.sentine.next = newNode;
         this.size++;
-
     }
-    //T get(int index)：获取索引处元素（0为队首），空返回null，
-    public T get(int x){
-       if(this.size == 0)return null;
-        return findNode(x).item;
+    public boolean isEmpty() {
+        return size == 0;
     }
+    public int size() {
+        return size;
+    }
+    //可以重新它的toString
 
-//内部辅助方式搜索索引为i的元素，甚至是哨兵
-    private Node<T> findNode(int i){
-        if(i < -1 || i >= this.size){
-            throw new IndexOutOfBoundsException("无法搜索");
-        }//-1是哨兵节点
-        Node<T> curr = this.sentry;
-        for(int x = -1 ; x < i; x++){
-            curr = curr.next;
+    @Override
+    public String toString(){
+        String[] LinkPhase = new String[this.size];
+        for(int i = 0;i < this.size;i++){
+            LinkPhase[i] =  get(i).toString();
         }
-        return curr;
+        return "{" + String.join(",",LinkPhase) +"}";
     }
-//T removeFirst() / T removeLast()：删除并返回首/尾元素，空时返回null
-    public T removeFirst(){
-        if(this.size == 0)return null;
-        return remove(0);
+    public void printDeque(){
+        System.out.println(this);
+    }
+
+   public T removeFirst(){
+        if(isEmpty())return null;
+        Node<T> First = sentine.prev;
+        this.sentine.prev = First.next;
+         First.next.prev = this.sentine;
+         T item = First.item;
+         First.next = null;
+         First.prev = null;
+         size--;
+        return item;
     }
     public T removeLast(){
-        if(this.size == 0)return null;
-        return remove(this.size - 1);
+        if(isEmpty())return null;
+        Node<T> last = sentine.next;
+        Node<T> lastButOne = sentine.next.prev;
+        this.sentine.next = lastButOne;
+        lastButOne.next = this.sentine;
+        T item = last.item;
+        last.next = null;
+        last.prev = null;
+        size--;
+        return item;
     }
 
-
-
-
-//返回列表长度
-    public int size(){
-        return this.size;
-    }
-//删除
-    public T remove(int x){
-        if (x < 0 || x >= this.size){
-            throw new IndexOutOfBoundsException("删除越界");
+    public T get(int index){
+        if (index < 0 || index >= size) return null;
+        Node<T> curr = this.sentine.prev;
+        for (int i = 0;i < index;i++){
+            curr = curr.next;
         }
-        Node<T> deleteNode = findNode(x);
-        Node<T> prevNode = findNode(x - 1);
-        prevNode.next = deleteNode.next;
-        deleteNode.next = null;
-        if (x == this.size - 1)this.tail = prevNode;
-        this.size--;
-        return deleteNode.item;
+        return curr.item;
     }
-
-
 
     public static void main(String[] args){
         LinkedListDepque<Integer> test = new LinkedListDepque<>();
-        test.addLast(3);
-        test.addLast(4);
-        test.addLast(6);
-        test.addLast(8);
-        test.insert(100,3);
-        test.insert(120,0);
-        test.insert(90,2);
-        test.remove(6);
-        test.remove(0);
-        test.remove(2);
-        test.addFirst(20000);
+      test.addLast(1);
+       test.addLast(2);
+       test.addLast(3);
+        test.addLast(1);
+        test.addFirst(2);
+        test.addFirst(3);
+        int x = test.removeFirst();
+        System.out.println(test);
     }
+
+
 
 }
