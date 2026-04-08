7be46805009c968de5eef5e3af9fd5c64abf1843
suobaiyuhaoenyutianshi <2467395920@qq.com>
2026-04-08 14:51:53 +0800

    1

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -2,25 +2,42 @@ package gitlet;
 
 // TODO: any imports you need here
 
-import java.util.Date; // TODO: You'll likely use this in this class
+// TODO: 在此处导入你需要的任何类
+import javax.xml.crypto.Data;
+import java.util.Date; // TODO: 你可能会在这个类中使用它
 
-/** Represents a gitlet commit object.
- *  TODO: It's a good idea to give a description here of what else this Class
- *  does at a high level.
+/** 表示一个 gitlet 提交对象。
+ *  TODO: 在这里给出一个高层次描述，说明这个类还做什么其他事情。
  *
  *  @author TODO
  */
 public class Commit {
     /**
-     * TODO: add instance variables here.
+     * TODO: 在此处添加实例变量。
      *
-     * List all instance variables of the Commit class here with a useful
-     * comment above them describing what that variable represents and how that
-     * variable is used. We've provided one example for `message`.
+     * 在这里列出 Commit 类的所有实例变量，并在每个变量上方添加有用的注释，
+     * 描述该变量代表什么以及如何使用。我们为 `message` 提供了一个示例。
      */
-
-    /** The message of this Commit. */
+    /** 此提交的消息。提交时用户输入的消息，即备注*/
     private String message;
+    private Date timeStamp;
+    private String parent1;
+    private String parent2;
+    //把哈希值存下来以免反复计算
+    public String SHA;
+    public Commit(String message ,String parent1 , String parent2){
+        this.message = message;
+        this.parent1 = parent1;
+        this.parent2 = parent2;
+        timeStamp = new Date();
+        this.SHA = calSHA(message,parent1,parent2,this.timeStamp);
+    }
+    //自身SHA的计算
+    //其他地方也可以用
+    public String calSHA(String message,String parent1,String parent2 ,Date timeStamp){
+         String SHA = Utils.sha1(message,parent1,parent2,timeStamp);
+         return SHA;
+    }
 
-    /* TODO: fill in the rest of this class. */
+    /* TODO: 填写此类的其余部分。 */
 }
